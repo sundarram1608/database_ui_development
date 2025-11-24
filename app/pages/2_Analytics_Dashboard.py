448904619ba6b4b1949148ee11a2182406b0f401
Sundar Ram Subramanian <110481773+sundarram1608@users.noreply.github.com>
2025-11-23 22:47:34 -0700

    Add files via upload

---
+++ app/pages/2_Analytics_Dashboard.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from interface import *
+
+st.set_page_config(
+                    page_title="Northwind Analytics Dashboard",
+                    page_icon="📈",
+                    layout="wide",
+                    initial_sidebar_state="collapsed"
+                )
+
+# Main Function to Control Navigation
+def main():
+    st.title(":blue[Northwind DBMS Analytics Dashboard]")
+    create_tabs_for_analytics()
+
+    
+if __name__ == "__main__":
+    main()
